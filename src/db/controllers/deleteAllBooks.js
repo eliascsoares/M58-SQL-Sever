ed89ded3bf8e334d2a5b05767e16a551daa81970
eliascsoares <40612619+eliascsoares@users.noreply.github.com>
2024-10-18 11:33:44 +0100

    Added deleAllBooks function

--- src/db/controllers/deleteAllBooks.js
+++ src/db/controllers/deleteAllBooks.js
@@ -2,10 +2,14 @@ const Book = require("../models/bookModel");
 
 async function deleteAllBooks(req,res) {
     try {
-        
+        await Book.destroy({
+            where: {},
+            truncate: true
+        });
+        res.status(200).json({ message: "All books have been deleted" });
     } catch (error) {
         console.log(error);
-        
+        res.status(500).json({ message: "Error deleting all books" });
     }
 }
 
